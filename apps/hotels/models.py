584b71718f6770bbc5a44cb88b45b7353848ccb0
Alberto200420 <105571383+Alberto200420@users.noreply.github.com>
2024-09-10 17:36:54 -0600

    changed Hotel Model to return self.hotel_name

--- apps/hotels/models.py
+++ apps/hotels/models.py
@@ -6,3 +6,6 @@ def logo_directory(instace, filename):
 class Hotel(models.Model):
   hotel_name = models.CharField(max_length=100, unique=True, editable=True)
   logo =       models.ImageField(upload_to=logo_directory)
+
+  def __str__(self):
+    return self.hotel_name
