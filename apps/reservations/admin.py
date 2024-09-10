5e88273abfc63bb84ad79894c0fa52b364f0b109
Alberto200420 <105571383+Alberto200420@users.noreply.github.com>
2024-09-10 17:12:48 -0600

    Created   - admin.py of reservations, serviceDirectory files

--- apps/reservations/admin.py
+++ apps/reservations/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Reservation
 
-# Register your models here.
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+  list_display = ('reservation_name', 'service', 'status', 'booking_date', 'number_of_people')
+  list_filter = ('status', 'service')
+  search_fields = ('reservation_name', 'service__title')
+  readonly_fields = ('id',)
+  fieldsets = (
+    (None, {
+      'fields': ('id', 'reservation_name', 'service', 'user')
+    }),
+    ('Reservation Details', {
+      'fields': ('status', 'number_of_people', 'booking_date', 'length_of_stay')
+    }),
+  )
--- apps/serviceDirectory/admin.py
+++ apps/serviceDirectory/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Category, Service, Images
 
-# Register your models here.
+class ImagesInline(admin.TabularInline):
+  model = Images
+  extra = 1
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('category_name', 'hotel')
+  list_filter = ('hotel',)
+  search_fields = ('category_name', 'hotel__hotel_name')
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+  list_display = ('title', 'category', 'require_reservation', 'maximum_capacity')
+  list_filter = ('category', 'require_reservation')
+  search_fields = ('title', 'description')
+  inlines = [ImagesInline]
+  fieldsets = (
+    (None, {
+      'fields': ('title', 'category', 'description')
+    }),
+    ('Media', {
+      'fields': ('menu', 'header_image')
+    }),
+    ('Availability', {
+      'fields': ('availabilities', 'require_reservation', 'maximum_capacity')
+    }),
+    ('Additional Info', {
+      'fields': ('requirement',),
+      'classes': ('collapse',)
+    }),
+  )
+
+# We don't register Images model separately as it's managed inline with Service
