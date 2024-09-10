89cb2c1cd52f254d83f04b8219a70b99bb1b2eb7
Alberto200420 <105571383+Alberto200420@users.noreply.github.com>
2024-09-10 06:55:10 -0600

    Connected hotels app with core Created:   - admin view   - model

--- apps/hotels/models.py
+++ apps/hotels/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+def logo_directory(instace, filename):
+  return 'logos/{0}/{1}'.format(instace.hotel_name, filename)
+
+class Hotel(models.Model):
+  hotel_name = models.CharField(max_length=100, unique=True, editable=True)
+  logo = models.ImageField(upload_to=logo_directory)
