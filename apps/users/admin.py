df60743787d52666d0ec2509cf02008c38c503b4
Alberto200420 <105571383+Alberto200420@users.noreply.github.com>
2024-09-10 21:46:03 -0600

    users app admin.py modifyed

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from apps.hotels.models import Hotel
 from .models import UserAccount
 
 class UserAccountAdmin(UserAdmin):
-  list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
-  list_filter = ('is_active', 'is_staff', 'is_superuser')
-  search_fields = ('email', 'first_name', 'last_name')
+  list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'hotel_belonging')
+  list_filter = ('is_active', 'is_staff', 'is_superuser', 'hotel_belonging')
+  search_fields = ('email', 'first_name', 'last_name', 'hotel_belonging__hotel_name')
   ordering = ('email',)
     
   fieldsets = (
     (None, {'fields': ('email', 'password')}),
     ('Personal Info', {'fields': ('first_name', 'last_name')}),
+    ('Hotel Association', {'fields': ('hotel_belonging',)}),
     ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
     ('Important dates', {'fields': ('last_login',)}),
   )
@@ -18,8 +20,13 @@ class UserAccountAdmin(UserAdmin):
   add_fieldsets = (
     (None, {
       'classes': ('wide',),
-      'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+      'fields': ('email', 'first_name', 'last_name', 'hotel_belonging', 'password1', 'password2'),
     }),
   )
+
+  def formfield_for_foreignkey(self, db_field, request, **kwargs):
+    if db_field.name == "hotel_belonging":
+      kwargs["queryset"] = Hotel.objects.all().order_by('hotel_name')
+    return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(UserAccount, UserAccountAdmin)
